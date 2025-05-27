e2b66c597d015af266a619f8ff68ccc170101b35
AlexTran0899 <alextran0899@gmail.com>
2025-05-27 10:48:46 +0700

    add apache-airflow-providers-http to requirements.txt

---
+++ dags/stock_market.py
@@ -0,0 +1,14 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    start_date = datetime(2023,1 ,1),
+    schedule = '@daily',
+    catchup = False,
+    tags = ['stock_market']
+)
+
+def stock_market():
+    @task.sensor(poke_interval = 30, timeout = 600)
+
+stock_market()
