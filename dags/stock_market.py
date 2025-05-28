65a9e5498aa41e5055145097067603d239d71ced
AlexTran0899 <alextran0899@gmail.com>
2025-05-28 10:22:52 +0700

    store the result of fetching NVDA stock from the api inside of minio

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,7 +3,7 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 
 
 SYMBOL = 'NVDA'
@@ -22,31 +22,29 @@ def stock_market():
 
         api = BaseHook.get_connection('stock_api')
         url = f"{api.host}{api.extra_dejson['endpoint']}"
-        print(url)
         response = requests.get(url, headers=api.extra_dejson['headers'])
         condition = response.json()['finance']['result'] is None 
-        print(f"Sensor condition evaluated to: {condition}")
         return PokeReturnValue(is_done=condition, xcom_value=url)
     
 
     get_stock_prices = PythonOperator(
-        task_id = 'get_stock_price',
+        task_id = 'get_stock_prices',
         python_callable = _get_stock_prices,
         op_kwargs = {
             'url': '{{ ti.xcom_pull(task_ids="is_api_available")}}',
             'symbol': SYMBOL
-
         }
     )
 
     store_prices = PythonOperator(
         task_id = 'store_prices',
-        python_callable= _store_prices
-
-
+        python_callable= _store_prices,
+        op_kwargs = {
+            'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices")}}'
+        }
     )
 
     # notice that you dont neet to use the () with opertors, only with decorators 
-    is_api_available() >> get_stock_prices
+    is_api_available() >> get_stock_prices >> store_prices
 
 stock_market()
--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -1,14 +1,29 @@
 from airflow.hooks.base import BaseHook
 from minio import Minio
+from io import BytesIO
+
 def _get_stock_prices(url, symbol):
     import requests
     import json
-    url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
-    api = BaseHook.get_connection('stock_api')
-    response = requests.get(url, headers= api.extra_dejson['headers'])
-    return json.dumps(response.json()['chart']['result'][0])
 
-def _store_prices():
+    api = BaseHook.get_connection('stock_api')
+    url = f"{url}{symbol}?metrics=high&interval=1d&range=1y"
+    response = requests.get(url, headers=api.extra_dejson['headers'])
+
+    try:
+        data = response.json()
+        result = data['chart']['result']
+        if result:
+            return json.dumps(result[0])
+        else:
+            raise ValueError("No result found in chart data")
+    except Exception as e:
+        print(f"[ERROR] Failed to fetch or parse data: {e}")
+        raise
+
+def _store_prices(stock):
+    import json
+
     minio = BaseHook.get_connection('minio')
     client = Minio(
         endpoint=minio.extra_dejson['endpoint_url'],
@@ -17,4 +32,19 @@ def _store_prices():
         secure=False
     )
 
+    bucket_name = 'stock-market'
+    # check to see if that bucket exists in minio, you can go to localhost:9001 to check manually
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+    stock = json.loads(stock)
+    symbol = stock['meta']['symbol']
+    print(symbol)
+    data = json.dumps(stock, ensure_ascii=False).encode('utf8')
+    objw = client.put_object(
+        bucket_name=bucket_name, 
+        object_name=f'{symbol}/prices.json',
+        data=BytesIO(data),
+        length=len(data)
+    )
+    return f'{objw.bucket_name}/{symbol}'
  
